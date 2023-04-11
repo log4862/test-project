e3a4d1ca77eb5c9a3e2194f6bc28ae52a4aafa83
log4862 <bhlog4862@mail.com>
2023-04-11 22:49:32 +1000

    main

--- python-files/intro.py
+++ python-files/intro.py
@@ -1,4 +1,7 @@
 #python test
 print ('Hello World from VS code')
 
-print ('opened an existing repo and alak-bulak committed it. Success = repeat tomorrow')
+print ('opened an existing repo and alak-bulak committed it. Success = repeat today')
+name = input ("Hiya")
+print()
+print (name)
